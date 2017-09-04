6ae6b9253964f701c097552add29ba56d753c5e2
Caleb Smith <karagenit@outlook.com>
2017-09-04 19:44:50 -0400

    Add Arduino Enum/Structs

--- arduino/material-sorter/material-sorter.ino
+++ arduino/material-sorter/material-sorter.ino
@@ -1,3 +1,21 @@
+enum MaterialType {
+  Bolt,
+  Screw,
+  Nut,
+  Washer,
+  Other
+};
+
+struct Material {
+  enum MaterialType type;
+  double length;
+  double width; //aka inner diameter
+};
+
+struct Material materials[] {
+  {MaterialType::Bolt, 1, 3.0/8.0}
+};
+
 void setup() {
 
 }
