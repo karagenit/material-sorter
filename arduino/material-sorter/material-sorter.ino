ff48e5e29fac2c208ceac89a4f0fe9c438255e13
Caleb Smith <karagenit@outlook.com>
2017-09-04 22:14:08 -0400

    Move Struct/Enum to Header

--- arduino/material-sorter/material-sorter.ino
+++ arduino/material-sorter/material-sorter.ino
@@ -1,19 +1,4 @@
-enum MaterialType {
-  Bolt,
-  Screw,
-  Nut,
-  Washer,
-  Other
-};
-
-struct Material {
-  enum MaterialType type;
-  double length;
-  double width; //aka inner diameter
-  bool operator==(const Material& other) {
-    return this->type == other.type && this->length == other.length && this->width == other.width;
-  }
-};
+#include "materials.h"
 
 //how many sections are on the belt, starting at the image sensor, ending at the end of the belt
 #define BELT_LEN 3
