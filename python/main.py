f7c67e289e594ba55a7ea14c8cfe4ccb145b379c
Caleb Smith <karagenit@outlook.com>
2018-04-12 11:49:30 -0400

    Add OpenCV Import

--- python/main.py
+++ python/main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 import arduino
+import vision
 
 run = True
 while run:
     arduino.await_signal()
     # grab image
+    vision.read_image()
     # process image
     # print results
     print("Image Processed")
+    run = False
---
+++ python/vision.py
@@ -0,0 +1,5 @@
+import numpy as np
+import cv2 as cv
+
+def read_image():
+    img = cv.imread('opencv.jpg', 0)
