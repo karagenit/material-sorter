1ed47895a9a950cbb4388e03b3399090290962b4
Caleb Smith <karagenit@outlook.com>
2018-04-12 12:11:59 -0400

    Add Color Range Filtering

--- python/vision.py
+++ python/vision.py
@@ -2,7 +2,12 @@ import numpy as np
 import cv2 as cv
 
 def read_image():
-    img = cv.imread('opencv.jpg', 0)
-    cv.imshow('image', img)
+    img = cv.imread('opencv.jpg')
+    #img = cv.cvtColor(img, cv.COLOR_RGB2HSV)
+    lower = np.array([100, 0, 0])
+    upper = np.array([255, 100, 100])
+    mask = cv.inRange(img, lower, upper)
+    cv.imshow('Original', img)
+    cv.imshow('BGR Filter', mask)
     cv.waitKey(0)
     cv.destroyAllWindows()
