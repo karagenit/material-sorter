beed041c3fa771d427ba74c132ad4c926bb954a8
Caleb Smith <karagenit@outlook.com>
2018-04-15 14:42:59 -0400

    Add Vision Constants

--- python/vision.py
+++ python/vision.py
@@ -1,6 +1,21 @@
 import numpy as np
 import cv2 as cv
 
+# This is used for calculating bolt info based on the processed image.
+
+# Pixels Per Inch - this is currently theoretical based on a 1920x1080p camera
+#   held 6 inches away from the table with a 90 degree horizontal FOV. (90 degree FOV @6" gives
+#   12" across camera horizontally, so 1920/12). Not sure how this will handle bolts not facing
+#   horizontally.
+PPI = 160
+
+# This determines how precisely we should try to sort/categorize bolts (e.g. by 1/4" lengths)
+#   Higher precision results in less accuracy (higher rate of false sorting).
+LENGTHS = [ 1, 1.25, 1.5, 1.75, 2 ]
+
+# Just #10 and 1/4"
+DIAMETERS = [ 0.19, 0.25 ]
+
 class Bolt:
     def __init__(self, length, diameter, threads):
         self.length = length
@@ -11,6 +26,7 @@ class Bolt:
 def read_image():
     return cv.imread('opencv.jpg')
 
+# TODO: filter to keep everything not green?
 def filter_image(original):
     #img = cv.cvtColor(img, cv.COLOR_RGB2HSV)
     lower = np.array([100, 0, 0])
