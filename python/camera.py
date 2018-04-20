158125fa77253245c08a43d96449287fcbd08ade
Caleb Smith <karagenit@outlook.com>
2018-04-20 15:49:05 +0000

    Add Global Declarations

--- python/camera.py
+++ python/camera.py
@@ -7,11 +7,15 @@ camera = None
 rawCapture = None
 
 def init():
+    global camera
+    global rawCapture
     camera = PiCamera()
     rawCapture = PiRGBArray(camera)
     time.sleep(0.1)
     return
 
 def read():
+    global camera
+    global rawCapture
     camera.capture(rawCapture, format="bgr")
     return rawCapture.array
