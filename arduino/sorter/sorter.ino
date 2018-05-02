6d2354f7070c114937694bb8a02da4ba4c39b30d
Caleb Smith <karagenit@outlook.com>
2018-05-02 09:15:09 -0400

    Add Arduino Servo Control

---
+++ arduino/sorter/sorter.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+
+#define BINS 10 // indexes 0..n-2, n-1 is overflow
+#define LED 13
+#define SERVO 3 //used for positioning chute
+#define SERVO_ANGLE 15
+
+Servo positionServo;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  positionServo.attach(SERVO);
+}
+
+void loop() {
+  while (Serial.available() < 1);
+  
+  byte bin = Serial.read();
+  
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+}
+
+// Bins start at index 0
+void setPosition(int bin) {
+  // set overflow bin
+  if (bin > BINS - 1) {
+    bin = BINS - 1;
+  }
+  // TODO servo angle offset
+  positionServo.write(bin * SERVO_ANGLE);
+}
