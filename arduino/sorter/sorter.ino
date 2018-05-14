19ec9e26feb70362a89827c09d5fb03aa95fb638
Caleb Smith <karagenit@outlook.com>
2018-05-13 20:09:03 -0400

    Add Working Servo Control Code

--- arduino/sorter/sorter.ino
+++ arduino/sorter/sorter.ino
@@ -5,9 +5,11 @@
 #define SSDB 9
 #define SSDC 10
 #define SSDD 11
-#define SERVO 3 //used for positioning chute
-#define SERVO_ANGLE 15
-
+#define SERVO 5 //used for positioning chute.. max write(5..180)
+#define SERVO_ANGLE_OFF 5
+#define SERVO_ANGLE_INC 15
+#define MOTOR 5 //used for spinning turntable
+#define TURN_TIME 500 // time to run the turntable motor for
 Servo positionServo;
 
 void setup() {
@@ -16,28 +18,43 @@ void setup() {
   pinMode(SSDB, OUTPUT);
   pinMode(SSDC, OUTPUT);
   pinMode(SSDD, OUTPUT);
+  pinMode(MOTOR, OUTPUT);
   positionServo.attach(SERVO);
 }
 
 void loop() {
-  while (Serial.available() < 1);
-  
-  byte bin = Serial.read();
-  
-  //setPosition(bin);
+  // Position table for next bolt
+  //moveTable();
 
+  // Signal Pi to Capture Image
+  //Serial.write(0x01);
+
+  // Wait for byte available and read it
+  while (Serial.available() < 1);
+  byte bin = Serial.read();
+
+  // set overflow bin
+  if (bin > BINS - 1) {
+    bin = BINS - 1;
+  }
+
+  // Set Chute position
+  setPosition(bin);
+
+  // Write Bin # to SSD
   digitalWrite(SSDA, bin & 0b0001);
   digitalWrite(SSDB, bin & 0b0010);
   digitalWrite(SSDC, bin & 0b0100);
   digitalWrite(SSDD, bin & 0b1000);
 }
 
+void moveTable() {
+  digitalWrite(MOTOR, HIGH);
+  delay(TURN_TIME);
+  digitalWrite(MOTOR, LOW);
+}
+
 // Bins start at index 0
 void setPosition(int bin) {
-  // set overflow bin
-  if (bin > BINS - 1) {
-    bin = BINS - 1;
-  }
-  // TODO servo angle offset
-  positionServo.write(bin * SERVO_ANGLE);
+  positionServo.write(SERVO_ANGLE_OFF + (bin * SERVO_ANGLE_INC));
 }
